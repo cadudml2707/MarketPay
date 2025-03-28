895c755c1e31e6aabcb1589e5a95c74e8bd8c53b
carlos.lima <gbdev11@gbringel.com>
2025-03-28 13:30:29 -0400

    Criando pasta de classes e adicionando a de Produto.

--- backend/src/App.java
+++ backend/src/App.java
@@ -1,5 +1,6 @@
+import javax.swing.JOptionPane;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        JOptionPane.showMessageDialog(null, "Hello World!");
     }
 }
