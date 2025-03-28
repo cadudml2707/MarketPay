8527741401382bdfed1d1d2807f51cf3535e5dc8
carlos.lima <gbdev11@gbringel.com>
2025-03-28 13:30:29 -0400

    Criando pasta de classes e adicionando a de Produto.

---
+++ backend/src/classes/Produto.java
@@ -0,0 +1,16 @@
+package classes;
+
+public class Produto {
+    
+    private int id;
+    private String nome;
+    private float preco;
+    private int quantidade; 
+
+    public Produto(int id, String nome, float preco, int quantidade){
+        this.id=id;
+        this.nome=nome;
+        this.preco=preco;
+        this.quantidade=quantidade;
+    }
+}
