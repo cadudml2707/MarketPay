e3ca461aa0ee766e543616239f6b1caf59f5e466
carlos.lima <carloseduardodml@gmail.com>
2025-03-29 21:53:14 -0400

    CRUD de Clientes

---
+++ backend/src/dao/ClienteDAO.java
@@ -0,0 +1,39 @@
+package dao;
+
+import classes.Cliente;
+import java.util.List;
+import java.util.ArrayList;
+
+public class ClienteDAO {
+    
+    List<Cliente> clientes = new ArrayList<>();
+
+    public void novoCliente(String nome, String cpf, String telefone){
+        Cliente cliente = new Cliente(nome, cpf, telefone);
+        clientes.add(cliente);
+    }
+
+    public void buscarCliente(String cpf){
+        for(Cliente cliente : clientes){
+            if(cliente.getCpf() == cpf){
+                cliente.getNome();
+                cliente.getCpf();
+                cliente.getTelefone();
+            }
+        }
+    }
+
+    public void atualizarCliente(String nome, String cpf, String telefone){
+        for(Cliente cliente : clientes){
+            if(cliente.getCpf() == cpf){
+                cliente.setNome(nome);
+                cliente.setCpf(cpf);
+                cliente.setTelefone(telefone);
+            }
+        }
+    }
+
+    public void deletarCliente(String nome, String cpf, String telefone){
+        clientes.removeIf(cliente -> cliente.getCpf() == cpf);
+    }
+}
