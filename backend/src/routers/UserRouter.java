c9fe76d960ba5da014c4fc4b1bbf74e79e32f341
carlos.lima <gbdev11@gbringel.com>
2025-05-26 17:05:48 -0400

    Configuração inicial das rotas do crud de usuários.

--- backend/src/routers/UserRouter.java
+++ backend/src/routers/UserRouter.java
@@ -9,9 +9,21 @@ public class UserRouter implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        String response = "Rota de usuários acessada";
-        exchange.sendResponseHeaders(200, response.getBytes().length);
-        try (OutputStream os = exchange.getResponseBody()) {
+        String method = exchange.getRequestMethod();
+        String response = "";
+
+        switch (method){
+            case "GET":
+                response = "Você usou GET (Listar usuários)";
+                break;
+        }
+
+        exchange.sendResponseHeaders(200, response.length());
+        send(exchange, response);
+    }
+
+    private void send(HttpExchange exchange, String response) throws IOException {
+        try (OutputStream os = exchange.getResponseBody()){
             os.write(response.getBytes());
         }
     }
