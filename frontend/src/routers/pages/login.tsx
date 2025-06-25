59ef552e2b82f3edc5e7dc201606055d9eeeabb7
carlos.lima <gbdev11@gbringel.com>
2025-06-25 17:39:37 -0400

    Configuração do shadcn

--- frontend/src/routers/pages/login.tsx
+++ frontend/src/routers/pages/login.tsx
@@ -1,5 +1,14 @@
+import Logo from '../../assets/react.svg';
+
 export function Login(){
     return(
-        <div>opaaakjhsfdjbhjskbnakhjcjsbvnjhfvhbmnkhjcvbndhjvcbnnsjhdbcvdshjfbmshkjabmvcxhjksbnmvcjkhdsbnmfjhbnm</div>
+        <div className="bg-red-200 justify-center content-center items-center">
+            <div className="flex-col">
+                <img src={Logo} alt="logo-market-pay" className='' />
+                <div className='justify-center'>
+                    
+                </div>
+            </div>
+        </div>
     );
 }
