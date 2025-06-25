f673eea5cb7f4e2e1e7bc2c1ff7c5721ed435280
carlos.lima <gbdev11@gbringel.com>
2025-06-25 16:46:44 -0400

    Instalação do tailwind e estruturação das rotas no frontend.

---
+++ frontend/src/routers/AppRoutes.tsx
@@ -0,0 +1,10 @@
+import { Route, Routes } from "react-router-dom";
+import { Login } from "./pages/login";
+
+export function AppRoutes(){
+    return(
+        <Routes>
+            <Route path="/" element={<Login/>} />
+        </Routes>
+    );
+}
