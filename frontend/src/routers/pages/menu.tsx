329f0e5f7e36b335f11d9c92d816b1fa386ffbb6
carlos.lima <gbdev11@gbringel.com>
2025-06-30 14:53:34 -0400

    Adicionando sidebar

---
+++ frontend/src/routers/pages/menu.tsx
@@ -0,0 +1,10 @@
+import Layout from '../../components/layout/layout';
+
+export function Menu(){
+    return(
+        <Layout>
+            <h1>MenuPrincipal</h1>
+            <p>conteúdo aqui</p>
+        </Layout>
+    );
+}
